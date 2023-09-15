eba80ba53af8bf35dec7f59a242ead0b968e2872
dbrettcom <69137389+dbrettcom@users.noreply.github.com>
2023-09-15 14:38:42 +0930

    Update platform1.v

--- platform1.v
+++ platform1.v
@@ -1,8 +1,8 @@
 module platform1(
-    input wire p1_on,				    // signal from platform3 module to initiate platform1 module
-    input wire clk,     			  // 50 MHz
-	  output reg [3:0] GPIO_1,		// output to the motor turning the platform
-	  output reg colour_sensor_on	// output to colour sensor informing it that there is a new M&M under the sensor
+    input wire p1_on,			// signal from platform3 module to initiate platform1 module
+    input wire clk,              	// 50 MHz
+    output reg [3:0] GPIO_1,		// output to the motor turning the platform
+    output reg colour_sensor_on		// output to colour sensor informing it that there is a new M&M under the sensor
 );
 
 // Need to instantiate colour sensor module here
@@ -20,11 +20,11 @@ reg [31:0] cntr = 0;
 reg [31:0] timer = (50_000_000 * 0.125); // Rotates M&M 45deg CW to sensor
 
 always @(posedge clk) begin
-    p1_on_internal <= p1_on; 			  // Store p1_on signal
+    p1_on_internal <= p1_on; 	        // Store p1_on signal
 
     if (p1_on_internal == 1) begin 	// Check if p1 module ready to begin
       
-	  p1_on_internal <= 0; 				    // Then change back to 0 again
+    p1_on_internal <= 0; 	        // Then change back to 0 again
       
     if (timer > 0) begin
         if (cntr < count*1/4) begin
